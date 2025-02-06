bac50e2b574d85310cbf641618387671abbc9905
Pablo George <pablogeokar@hotmail.com>
2025-02-06 19:25:49 -0300

    fazendo as configurações iniciais da api

---
+++ frontend/src/lib/axios-client.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const options = {
+  baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+  withCredentials: true,
+  timeout: 10000,
+};
+
+const API = axios.create(options);
+
+API.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const { data, status } = error.response;
+    if (data === "Unauthorized" && status === 401) {
+    }
+    return Promise.reject({ ...data });
+  }
+);
